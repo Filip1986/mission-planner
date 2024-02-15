684dff15e75841fb38b994378e35b74a83efb673
Luca Filip <filip.luca.work@gmail.com>
2024-02-15 18:21:57 +0200

    MPL-1002 Add functionalities for navigation, share and logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home', 
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path: 'planner', 
+        loadComponent: () => import('./planner/planner.component').then(m => m.PlannerComponent)
+    },
+    {
+        path: 'player', 
+        loadComponent: () => import('./player/player.component').then(m => m.PlayerComponent)
+    }
+];
